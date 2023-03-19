e8051e28e6547daa713d9305fba08029104ded1f
Mark Lee <mlee.aus@gmail.com>
2023-03-19 14:48:44 +1100

    feat: add code to create tasks and function to call Google's update phoeo REST endpoint

--- main.ts
+++ main.ts
@@ -24,7 +24,119 @@ const users = await getUsers(
   500,
 )
 
-console.log(users)
+interface Task {
+  primaryEmail: string
+  workspaceId: string
+  photoData: string
+  etag: string
+}
+
+const tasks: Task[] = []
+
+for await (const f of Deno.readDir(appSettings.imagePath)) {
+  if (f.isFile) {
+    const userName = `${
+      f.name.split('.')[0].toLowerCase()
+    }@${appSettings.domain}`
+
+    if (!users.has(userName) || users.get(userName).suspended) {
+      continue
+    }
+
+    const workspaceId = users.get(userName).id
+    const primaryEmail = users.get(userName).email
+
+    const file = await Deno.readFile(`${appSettings.imagePath}/${f.name}`)
+    const photoData = base64url.encode(file)
+
+    const md5 = await crypto.subtle.digest(
+      'MD5',
+      new TextEncoder().encode(photoData),
+    )
+
+    const etag = toHashString(md5)
+
+    const task: Task = {
+      primaryEmail,
+      workspaceId,
+      photoData,
+      etag,
+    }
+    tasks.push(task)
+  }
+}
+
+runTasks(tasks)
+
+async function runTasks(tasks: Task[]) {
+  await Promise.all(
+    tasks.map(async (task, index) => {
+      await updatePhoto(
+        auth,
+        task.primaryEmail,
+        task.workspaceId,
+        task.photoData,
+        task.etag,
+        index,
+        tasks.length,
+      )
+    }),
+  )
+}
+
+async function updatePhoto(
+  auth: GoogleAuth,
+  userId: string,
+  workspaceId: string,
+  photoData: string,
+  etag: string,
+  index: number,
+  total: number,
+) {
+  index = index + 1
+  const encodedUserId = encodeURIComponent(userId)
+  const url =
+    `https://admin.googleapis.com/admin/directory/v1/users/${encodedUserId}/photos/thumbnail`
+
+  const delay = index * appSettings.taskDelay
+  await sleep(delay)
+
+  console.log(`Setting user photo for ${userId} - ${index} of ${total} tasks`)
+
+  const body = JSON.stringify({
+    id: workspaceId,
+    primaryEmail: userId,
+    kind: 'admin#directory#user#photo',
+    etag: etag,
+    photoData: photoData,
+    mimeType: 'JPEG',
+    width: 250,
+    height: 250,
+  })
+
+  try {
+    const response = await fetch(
+      url,
+      {
+        method: 'PUT',
+        headers: getHeaders(auth),
+        body,
+      },
+    )
+
+    if (response.status === 200) {
+      console.log(
+        `%c[ Set user photo for ${userId} - Status: ${response.status} ok ]\n`,
+        'color:green',
+      )
+    } else {
+      throw `Status: ${response.status} exiting script`
+    }
+  } catch (e) {
+    console.log(e)
+    Deno.exit(1)
+  }
+}
 
 async function getUsers(
   auth: GoogleAuth,
@@ -86,4 +198,8 @@ function getHeaders(auth: GoogleAuth) {
     'content-type': 'application/json',
     'accept': 'application/json',
   }
+}
+
+function sleep(delay: number) {
+  return new Promise((resolve) => setTimeout(resolve, delay))
 }
